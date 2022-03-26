1c52250943ada851b97e5547eaae0d66e1506485
dev <eloper>
2022-03-26 16:40:09 +0500

    на сайте выводится структура ответов

--- draw_post_tree.py
+++ draw_post_tree.py
@@ -4,6 +4,10 @@ from data import db_session
 db_session.global_init("db/borda.db")
 db_sess = db_session.create_session()
 
+tree = {0: {}}
+posts = {0: Post()}
+format_posts = []
+
 
 def walk(tree, deep=0):
     if len(tree.keys()) == 0:
@@ -30,35 +34,23 @@ def walk_and_show(tree, deep=0):
     if len(tree.keys()) == 0:
         return
     for k, v in tree.items():
-        print('\t' * deep, posts[k].title, posts[k].content)
+        # print('\t' * deep, posts[k].title, posts[k].content)
+        format_posts.append((deep, posts[k]))
         # print('\t' * deep, 'ответы:')
         walk_and_show(v, deep=deep + 1)
         # print('\t' * deep, '}')
     return
 
 
-# tree = {
-#     0: {
-#         1: {
-#             2: {
-#                 4: {
-#                 }
-#             },
-#             3: {
-#             }
-#         },
-#         5: {
-#         }
-#     }
-# }
+def get_format_posts():
+    if not format_posts:
+        for post in db_sess.query(Post).all():
+            walk_and_push(tree, post.reply_to_id, post.id)
+            posts[post.id] = post
+        # print(tree)
+        walk_and_show(tree)
 
-tree = {0: {}}
-posts = {0: Post()}
+        # for i in format_posts:
+        #     print(i)
 
-for post in db_sess.query(Post).all():
-    walk_and_push(tree, post.reply_to_id, post.id)
-    posts[post.id] = post
-# print(tree)
-
-
-walk_and_show(tree)
+    return format_posts
--- server.py
+++ server.py
@@ -3,14 +3,14 @@ from data import db_session
 from data.posts import Post
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'in_fact_we_are_not_powerless_but_weak-willed._will_will_make_any_choice_right'
+app.config['SECRET_KEY'] = 'in_fact_we_are_not_powerless_but_weak-willed__will_will_make_any_choice_right'
 
 
 @app.route("/")
 def index():
-    db_sess = db_session.create_session()
-    posts = db_sess.query(Post)
-    return render_template("main.html", posts=posts)
+    from draw_post_tree import get_format_posts
+    format_posts = get_format_posts()
+    return render_template("main.html", format_posts=format_posts)
 
 
 def main():
