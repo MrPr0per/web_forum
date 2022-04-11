0450b5f004beb6a9752afac34298f47bcdf6e3ce
mr_goldenman <vasya.chaukin.97@mail.ru>
2022-04-11 22:09:40 +0500

    готов к загрузке на хостинг(опять)

--- server.py
+++ server.py
@@ -109,10 +109,10 @@ def index2(db_section):
 def main():
     db_session.global_init("db/borda.db")
     # for self
-    app.run(port=8080, host='127.0.0.1')
+    #app.run(port=8080, host='127.0.0.1')
 # for internet
-    #port = int(os.environ.get("PORT",5000))
-    #app.run(port=port, host='0.0.0.0')
+    port = int(os.environ.get("PORT",5000))
+    app.run(port=port, host='0.0.0.0')
 
 if __name__ == '__main__':
     main()
