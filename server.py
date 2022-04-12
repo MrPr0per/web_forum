dc57853b9a760f4b277e0e612a36d8b06dad5f2d
mr_goldenman <vasya.chaukin.97@mail.ru>
2022-04-12 18:04:11 +0500

    быстрофикс №5

--- server.py
+++ server.py
@@ -119,9 +119,11 @@ def main():
     # for self
     #app.run(port=8080, host='127.0.0.1')
     # for internet
+    #
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
     main()
+
