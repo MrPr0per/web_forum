540f5a76bf02c62702d99a54dbe4b8000df88888
mr_goldenman <vasya.chaukin.97@mail.ru>
2022-04-14 18:02:19 +0500

    добавлена капча(update2)

--- server.py
+++ server.py
@@ -122,11 +122,11 @@ def index2(db_section):
 def main():
     db_session.global_init("db/borda.db")
     # for self
-    app.run(port=8080, host='127.0.0.1')
+    #app.run(port=8080, host='127.0.0.1')
     # for internet
     #
-    #port = int(os.environ.get("PORT", 5000))
-    #app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
